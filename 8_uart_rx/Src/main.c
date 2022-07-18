1b6a4fac231468cf321f40cfd21e636e3d2865f9
Narensraj <68376669+Narensraj@users.noreply.github.com>
2022-07-18 13:21:21 -0400

    Add files via upload

---
+++ 8_uart_rx/Src/main.c
@@ -0,0 +1,34 @@
+//UART transmitter driver using printf
+
+#include "uart.h"
+
+#define GPIOA_5		(1U<<5)
+#define GPIOAEN		(1U<<0)
+
+#define LED_pin		GPIOA_5
+
+char key;
+
+int main(void)
+{
+
+	RCC->AHB1ENR |= GPIOAEN;
+
+	GPIOA->MODER |= (1U<<10);
+	GPIOA->MODER &=~(1U<<11);
+
+	uart_rxtx_init();								//UART transmit initialization
+
+	while(1)
+	{
+		key = uart2_read();
+
+		if(key == '1')
+			GPIOA->ODR |= LED_pin;
+		else
+			GPIOA->ODR &=~ LED_pin;
+	}
+
+}
+
+
