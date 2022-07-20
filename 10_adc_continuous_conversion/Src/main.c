be6aa4ac258375c0be47b77a58a32a8f3a8c99ec
Narensraj <68376669+Narensraj@users.noreply.github.com>
2022-07-20 09:15:24 -0400

    Add files via upload

---
+++ 10_adc_continuous_conversion/Src/main.c
@@ -0,0 +1,23 @@
+//ADC for continuous conversion
+
+#include "stm32f4xx.h"
+#include<stdint.h>
+#include "uart.h"
+#include "adc.h"
+
+uint32_t sensor_value;
+int main(void)
+{
+
+
+	uart_rxtx_init();								//UART transceiver initialization
+	pa1_adc_int();
+	start_conversion();
+	while(1)
+	{
+		sensor_value = adc_read();
+		printf("Sensor value : %d",(int)sensor_value);
+}
+}
+
+
