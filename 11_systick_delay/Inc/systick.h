fa4e43254dc695d6d6d9a30c2aa5536a8d672c1b
Narensraj <68376669+Narensraj@users.noreply.github.com>
2022-07-21 09:08:06 -0400

    Add files via upload

---
+++ 11_systick_delay/Inc/systick.h
@@ -0,0 +1,13 @@
+/*
+ * systick.h
+ *
+ *  Created on: Jul. 20, 2022
+ *      Author: Naren Subburaj
+ */
+
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+
+void systickDelayMs(int delay);
+
+#endif /* SYSTICK_H_ */
