31942ddf91898ed651982350acdeb8254ddcffa6
Narensraj <68376669+Narensraj@users.noreply.github.com>
2022-08-06 00:13:03 -0400

    Add files via upload

---
+++ 17_adc_interrupt/Inc/adc.h
@@ -0,0 +1,19 @@
+/*
+ * adc.h
+ *
+ *  Created on: Jul. 20, 2022
+ *      Author: Naren Subburaj
+ */
+
+#ifndef ADC_H_
+#define ADC_H_
+
+#include "stm32f4xx.h"
+#include<stdint.h>
+
+void pa1_adc_int(void);
+uint32_t adc_read();
+void start_conversion(void);
+void pa1_adc_interrupt_int(void);
+
+#endif /* ADC_H_ */
